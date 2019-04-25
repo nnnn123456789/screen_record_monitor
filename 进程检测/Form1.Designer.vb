dbb0c32869f41b000401d8d7b541dac744757c87
Yuan Jaw-Ming <nnnn123456789@mail.bnu.edu.cn>
2019-04-25 15:18:23 +0800

    create the solution

---
+++ 进程检测/Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(164, 54)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "开始运行"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(182, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(164, 54)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "更改录屏进程名"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "监测录屏程序"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(182, 132)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(164, 52)
+        Me.ListBox1.TabIndex = 4
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 72)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(164, 54)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "添加监控进程"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(182, 72)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(164, 54)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "删除选中的监控"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 132)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(164, 52)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "从文件读取"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(360, 201)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "录屏监测程序"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ 进程检测/Form1.vb
@@ -0,0 +1,142 @@
+﻿
+
+Public Class Form1
+    Dim MonitorName As String = ""
+    Public flagR As Boolean, flagP As Boolean, flagE As Boolean
+    Public Function GetProcNames() As String()
+        Dim processes = System.Diagnostics.Process.GetProcesses()
+        Dim prcNames(processes.Length) As String
+        For i = 0 To processes.Length - 1
+            Dim Thetext = processes(i).ToString()
+            Thetext = Thetext.Substring(Thetext.IndexOf("(") + 1)
+            Thetext = Mid(Thetext, 1, Thetext.Length - 1)
+            prcNames(i) = Thetext.Trim
+        Next i
+        Return prcNames
+    End Function
+    Public Sub Listen()
+        Dim prcNames As String() = GetProcNames()
+        For Each thename In prcNames
+            If thename = MonitorName Then
+                flagR = True
+            End If
+        Next
+        If flagR = False Then
+            Sed()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MonitorName = InputBox("请输入录屏进程名, 不含"".exe""", MonitorName)
+    End Sub
+
+    Sub Readfile(ByRef FileName As String)
+        Dim strData As String()
+        Try
+            strData = System.IO.File.ReadAllLines(FileName)
+        Catch e As Exception
+            MsgBox("读取文件失败")
+            Exit Sub
+        End Try
+
+        ListBox1.Items.Clear()
+        For Each s In strData
+            If s = Nothing Then Continue For
+            ListBox1.Items.Add(s)
+        Next
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Listen()
+    End Sub
+
+    Function Start() As Boolean
+        If MonitorName = "" Then
+            MsgBox("获取录屏进程名失败")
+            Return False
+        Else
+            Timer1.Enabled = True
+            Button1.Text = "停止运行"
+            Return True
+        End If
+    End Function
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Timer1.Enabled = True Then
+            Timer1.Enabled = False
+            Button1.Text = "开始运行"
+        Else
+            Start()
+        End If
+    End Sub
+
+    Public Sub Sed()
+        Dim prcNames As String() = GetProcNames()
+
+        For Each thename In prcNames
+            If thename = Nothing Then Continue For
+            If ListBox1.Items.IndexOf(thename) <> -1 Then
+                Timer1.Enabled = False
+                MsgBox("请运行录屏!", MsgBoxStyle.OkOnly + MsgBoxStyle.SystemModal, "注意")
+                Timer1.Enabled = True
+                Exit For
+            End If
+        Next
+        If flagP = True Or flagE = True Then
+            Timer1.Enabled = False
+            MsgBox("请运行录屏!", MsgBoxStyle.OkOnly + MsgBoxStyle.SystemModal, "注意")
+            Timer1.Enabled = True
+        End If
+    End Sub
+
+    Private Sub BtnDebug_Click(sender As Object, e As EventArgs)
+        Listen()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim Name = InputBox("请输入想要监控的进程名, 不含"".exe""", "")
+        Name = Name.Trim
+        If (Name = "") Then Return
+        If ListBox1.FindString(Name) <> -1 Then Return
+        ListBox1.Items.Add(Name)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Add("POWERPNT")
+        ListBox1.Items.Add("MicrosoftEdge")
+        ListBox1.Items.Add("devenv")
+        ListBox1.Items.Add("gcc")
+        ListBox1.Items.Add("javac")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If (ListBox1.SelectedItem <> -1) Then ListBox1.Items.Remove(ListBox1.SelectedItem)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim Filename = InputBox("请输入监控列表").Trim()
+        Readfile(Filename)
+    End Sub
+
+    Private Sub Form1_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Resize
+
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Hide()
+        End If
+
+        '这里实现窗口不允许更改大小（就是在改变窗口大小的时候锁定某一数值，实现无法改变大小的效果。另外要把最大化按钮设置为false）
+        Me.Width = 376
+        Me.Height = 240
+
+    End Sub
+    Private Sub NotifyIcon1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.DoubleClick
+
+        Me.ShowInTaskbar = True
+        Me.Show()
+
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+End Class
